6508dad6658ee0f0022ee0ec5520208eab6d3d25
niedziolkadadrian <niedziolkadadrian@gmail.com>
2021-06-22 19:12:04 +0200

    First tests

---
+++ Assets/Scripts/HolePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetHole();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResetHole() {
+        transform.position = new Vector3(Random.Range(.0f,8.0f),0,0);
+    }
+}
